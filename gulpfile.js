25cbf0ec1b53a209804fdc0afe95fa9009a7fe2d
Jim Zhang <jzhang@us.ibm.com>
2016-10-05 16:29:06 -0400

    Initial implementation for sending endorser proposal
    To test, set up a network from the feature/convergence branch using a v1.0
    docker-compose such as bddtests/bdd-docker/docker-compose-next-4.yml. Make
    sure to map the ports by adding the "ports" section.
    
    If you can't get this working in a native docker, you can run it inside
    vagrant.
    
    Run "node test/unit/endorser-tests.js"
    
    In response to comments:
    - merged the latest from master (changes from CR 1477)
    
    Change-Id: Icee9600580be1dd42c1d1b44d57906bf84c06c94
    Signed-off-by: Jim Zhang <jzhang@us.ibm.com>

--- gulpfile.js
+++ gulpfile.js
@@ -1,31 +1,8 @@
 var requireDir = require('require-dir');
+var gulp = require('gulp');
 
 // Require all tasks in gulp/tasks, including subfolders
 requireDir('./build/tasks', { recurse: true });
-
-var gulp = require('gulp');
-var eslint = require('gulp-eslint');
-
-gulp.task('lint', function () {
-	return gulp.src(['**/*.js', '!node_modules/**', '!docs/**'])
-		.pipe(eslint(
-			{
-				env: ['es6', 'node'],
-				extends: 'eslint:recommended',
-				parserOptions: {
-					sourceType: 'module'
-				},
-				rules: {
-					indent: ['error', 'tab'],
-					'linebreak-style': ['error', 'unix'],
-					quotes: ['error', 'single'],
-					semi: ['error', 'always']
-				}
-			}
-		))
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
-});
 
 gulp.task('default', ['lint'], function () {
 		// This will only run if the lint task is successful...
