fd3626b3ef001dd6a8421f9e30003db0989dc47b
Ratnakar <asara.ratnakar@gmail.com>
2017-01-26 11:42:46 -0500

    [FAB-1867] end-to-end based example node program
    This is same as end-to-end.js unit tests with out the tape fwk.
    It is split to deploy.js, invoke.js and query.js.
    This can be used for differenet chaincodes by modifying
    configuration values in config.json
    
    Change-Id: I2a3eaa0e015fa5269592e32ebc95dd78f56e48af
    Signed-off-by: Ratnakar <asara.ratnakar@gmail.com>

--- build/tasks/eslint.js
+++ build/tasks/eslint.js
@@ -2,34 +2,43 @@ var gulp = require('gulp');
 var eslint = require('gulp-eslint');
 
 gulp.task('lint', function () {
-	return gulp.src(['**/*.js', 'fabric-client/**/*.js', '!node_modules/**', '!docs/**', '!coverage/**', '!tmp/**', 'fabric-ca-client/lib/*.js'])
-		.pipe(eslint(
-			{
-				env: ['es6', 'node'],
-				extends: 'eslint:recommended',
-				parserOptions: {
-					sourceType: 'module'
-				},
-				rules: {
-					indent: ['error', 'tab'],
-					'linebreak-style': ['error', 'unix'],
-					quotes: ['error', 'single'],
-					semi: ['error', 'always'],
-					'no-trailing-spaces': ['error'],
-					'max-len': [
-						'error',
-						{
-							'code': 150,
-							'ignoreTrailingComments': true,
-							'ignoreUrls': true,
-							'ignoreStrings': true,
-							'ignoreTemplateLiterals': true,
-							'ignoreRegExpLiterals': true
-						}
-					]
-				}
+	return gulp.src([
+		'**/*.js',
+		'fabric-client/**/*.js',
+		'fabric-ca-client/lib/*.js',
+		'examples/**/*.js',
+		'!node_modules/**',
+		'!docs/**',
+		'!coverage/**',
+		'!tmp/**'
+	])
+	.pipe(eslint(
+		{
+			env: ['es6', 'node'],
+			extends: 'eslint:recommended',
+			parserOptions: {
+				sourceType: 'module'
+			},
+			rules: {
+				indent: ['error', 'tab'],
+				'linebreak-style': ['error', 'unix'],
+				quotes: ['error', 'single'],
+				semi: ['error', 'always'],
+				'no-trailing-spaces': ['error'],
+				'max-len': [
+					'error',
+					{
+						'code': 150,
+						'ignoreTrailingComments': true,
+						'ignoreUrls': true,
+						'ignoreStrings': true,
+						'ignoreTemplateLiterals': true,
+						'ignoreRegExpLiterals': true
+					}
+				]
 			}
-		))
-		.pipe(eslint.format())
-		.pipe(eslint.failAfterError());
+		}
+	))
+	.pipe(eslint.format())
+	.pipe(eslint.failAfterError());
 });
