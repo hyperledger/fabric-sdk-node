7a54782c12cd357ed438fabc75d953ef9e695279
Jim Zhang <jzhang@us.ibm.com>
2017-02-05 23:18:36 -0500

    Re-format end-to-end test with lambda
    This is a pretty complex application and using the lambda style
    callbacks makes the code look much cleaner and easier to follow
    
    Rebased with 5427
    
    Change-Id: I03d18614159fb1d4aa09db8e43b0261d7ef5fded
    Signed-off-by: Jim Zhang <jzhang@us.ibm.com>

--- test/unit/end-to-end.js
+++ test/unit/end-to-end.js
@@ -41,7 +41,6 @@ var EventHub = require('fabric-client/lib/EventHub.js');
 var client = new hfc();
 var chain = client.newChain('testChain-e2e');
 
-var webUser = null;
 var chaincode_id = 'end2end';
 var chain_id = 'testchainid';
 var tx_id = null;
@@ -67,10 +66,10 @@ chain.addOrderer(new Orderer('grpc://localhost:7050'));
 chain.addPeer(peer0);
 chain.addPeer(peer1);
 
-test('End-to-end flow of chaincode deploy, transaction invocation, and query', function(t) {
+test('End-to-end flow of chaincode deploy, transaction invocation, and query', (t) => {
 	hfc.newDefaultKeyValueStore({
 		path: testUtil.KVS
-	}).then( function (store) {
+	}).then((store) => {
 		client.setStateStore(store);
 		var promise = testUtil.getSubmitter(client, t);
 
@@ -80,7 +79,7 @@ test('End-to-end flow of chaincode deploy, transaction invocation, and query', f
 		eh.connect();
 
 		// override t.end function so it'll always disconnect the event hub
-		t.end = (function(context, eventhub, f) {
+		t.end = ((context, eventhub, f) => {
 			return function() {
 				if (eventhub && eventhub.isconnected()) {
 					logger.info('Disconnecting the event hub');
@@ -93,259 +92,210 @@ test('End-to-end flow of chaincode deploy, transaction invocation, and query', f
 
 		if (!useSteps || steps.indexOf('step1') >= 0) {
 			logger.info('Executing step1');
-			promise = promise.then(
-				function(admin) {
-					t.pass('Successfully enrolled user \'admin\'');
-					webUser = admin;
-					tx_id = utils.buildTransactionID({length:12});
-					nonce = utils.getNonce();
+			promise = promise.then((admin) => {
+				t.pass('Successfully enrolled user \'admin\'');
+				tx_id = utils.buildTransactionID({length:12});
+				nonce = utils.getNonce();
 
-					// send proposal to endorser
+				// send proposal to endorser
+				var request = {
+					chaincodePath: testUtil.CHAINCODE_PATH,
+					chaincodeId: chaincode_id,
+					fcn: 'init',
+					args: ['a', '100', 'b', '200'],
+					chainId: chain_id,
+					txId: tx_id,
+					nonce: nonce
+				};
+
+				return chain.sendDeploymentProposal(request);
+			},
+			(err) => {
+				t.fail('Failed to enroll user \'admin\'. ' + err);
+				t.end();
+			}).then((results) => {
+				var proposalResponses = results[0];
+
+				var proposal = results[1];
+				var header   = results[2];
+				var all_good = true;
+				for(var i in proposalResponses) {
+					let one_good = false;
+					if (proposalResponses && proposalResponses[0].response && proposalResponses[0].response.status === 200) {
+						one_good = true;
+						logger.info('deploy proposal was good');
+					} else {
+						logger.error('deploy proposal was bad');
+					}
+					all_good = all_good & one_good;
+				}
+				if (all_good) {
+					t.pass(util.format('Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s", metadata - "%s", endorsement signature: %s', proposalResponses[0].response.status, proposalResponses[0].response.message, proposalResponses[0].response.payload, proposalResponses[0].endorsement.signature));
 					var request = {
-						chaincodePath: testUtil.CHAINCODE_PATH,
-						chaincodeId: chaincode_id,
-						fcn: 'init',
-						args: ['a', '100', 'b', '200'],
-						chainId: chain_id,
-						txId: tx_id,
-						nonce: nonce
+						proposalResponses: proposalResponses,
+						proposal: proposal,
+						header: header
 					};
-
-					return chain.sendDeploymentProposal(request);
-				},
-				function(err) {
-					t.fail('Failed to enroll user \'admin\'. ' + err);
+					return chain.sendTransaction(request);
+				} else {
+					t.fail('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
 					t.end();
 				}
-			).then(
-				function(results) {
-					var proposalResponses = results[0];
-					//logger.debug('deploy proposalResponses:'+JSON.stringify(proposalResponses));
-					var proposal = results[1];
-					var header   = results[2];
-					var all_good = true;
-					for(var i in proposalResponses) {
-						let one_good = false;
-						if (proposalResponses && proposalResponses[0].response && proposalResponses[0].response.status === 200) {
-							one_good = true;
-							logger.info('deploy proposal was good');
-						} else {
-							logger.error('deploy proposal was bad');
-						}
-						all_good = all_good & one_good;
-					}
-					if (all_good) {
-						t.pass(util.format('Successfully sent Proposal and received ProposalResponse: Status - %s, message - "%s", metadata - "%s", endorsement signature: %s', proposalResponses[0].response.status, proposalResponses[0].response.message, proposalResponses[0].response.payload, proposalResponses[0].endorsement.signature));
-						var request = {
-							proposalResponses: proposalResponses,
-							proposal: proposal,
-							header: header
-						};
-						return chain.sendTransaction(request);
-					} else {
-						t.fail('Failed to send Proposal or receive valid response. Response null or status is not 200. exiting...');
-						t.end();
-					}
-				},
-				function(err) {
-					t.fail('Failed to send deployment proposal due to error: ' + err.stack ? err.stack : err);
-					t.end();
-				}
-			).then(
-				function(response) {
-					if (response.status === 'SUCCESS') {
-						t.pass('Successfully sent deployment transaction to the orderer.');
+			},
+			(err) => {
+				t.fail('Failed to send deployment proposal due to error: ' + err.stack ? err.stack : err);
+				t.end();
+			}).then((response) => {
+				if (response.status === 'SUCCESS') {
+					t.pass('Successfully sent deployment transaction to the orderer.');
 
-						// set the transaction listener and set a timeout of 30sec
-						// if the transaction did not get committed within the timeout period,
-						// fail the test
-						return new Promise((resolve, reject) => {
-							var handle = setTimeout(reject, 30000);
+					// set the transaction listener and set a timeout of 30sec
+					// if the transaction did not get committed within the timeout period,
+					// fail the test
+					return new Promise((resolve, reject) => {
+						var handle = setTimeout(reject, 30000);
 
-							eh.registerTxEvent(tx_id.toString(), (tx) => {
-								t.pass('The chaincode deploy transaction has been successfully committed');
-								clearTimeout(handle);
+						eh.registerTxEvent(tx_id.toString(), (tx) => {
+							t.pass('The chaincode deploy transaction has been successfully committed');
+							clearTimeout(handle);
 
-								if (!useSteps) {
-									resolve();
-								} else if (steps.length === 1 && steps[0] === 'step1') {
-									t.end();
-									resolve();
-								}
-							});
+							if (!useSteps) {
+								resolve();
+							} else if (steps.length === 1 && steps[0] === 'step1') {
+								t.end();
+								resolve();
+							}
 						});
-					} else {
-						t.fail('Failed to order the deployment endorsement. Error code: ' + response.status);
-						t.end();
-					}
-
-				},
-				function(err) {
-					t.fail('Failed to send deployment e due to error: ' + err.stack ? err.stack : err);
+					});
+				} else {
+					t.fail('Failed to order the deployment endorsement. Error code: ' + response.status);
 					t.end();
 				}
-			);
+
+			},
+			(err) => {
+				t.fail('Failed to send deployment e due to error: ' + err.stack ? err.stack : err);
+				t.end();
+			});
 		}
 
 		if (!useSteps || steps.indexOf('step2') >= 0) {
-			promise = promise.then(
-				function(data) {
-					logger.info('Executing step2');
+			promise = promise.then((data) => {
+				logger.info('Executing step2');
 
-					// we may get to this point from the sleep() call above
-					// or from skipping step1 altogether. if coming directly
-					// to this step then "data" will be the webUser
-					if (typeof data !== 'undefined' && data !== null) {
-						webUser = data;
-					}
+				tx_id = utils.buildTransactionID({length:12});
+				nonce = utils.getNonce();
+				// send proposal to endorser
+				var request = {
+					chaincodeId : chaincode_id,
+					fcn: 'invoke',
+					args: ['move', 'a', 'b','100'],
+					chainId: chain_id,
+					txId: tx_id,
+					nonce: nonce
+				};
+				return chain.sendTransactionProposal(request);
+			},
+			(err) => {
+				t.fail('Failed to get transaction notification within the timeout period');
+				t.end();
+			}).then((results) => {
+				var all_good = false;
+				if (results) {
+					var proposalResponses = results[0];
+					var proposal = results[1];
+					var header   = results[2];
 
-					return;
-				},
-				function(err) {
-					t.fail('Failed to get transaction notification within the timeout period');
-					t.end();
-				}
-			).then(
-				function() {
-					tx_id = utils.buildTransactionID({length:12});
-					nonce = utils.getNonce();
-					// send proposal to endorser
-					var request = {
-						chaincodeId : chaincode_id,
-						fcn: 'invoke',
-						args: ['move', 'a', 'b','100'],
-						chainId: chain_id,
-						txId: tx_id,
-						nonce: nonce
-					};
-					return chain.sendTransactionProposal(request);
-				},
-				function(err) {
-					t.fail('Failed to wait due to error: ' + err.stack ? err.stack : err);
-					t.end();
-				}
-			).then(
-				function(results) {
-					var all_good = false;
-					if (results) {
-						var proposalResponses = results[0];
-						var proposal = results[1];
-						var header   = results[2];
-
-						all_good = true;
-						for(var i in proposalResponses) {
-							let one_good = false;
-							if (proposalResponses && proposalResponses[i].response && proposalResponses[i].response.status === 200) {
-								one_good = true;
-								logger.info('move proposal was good');
-							} else {
-								logger.error('move proposal was bad');
-							}
-							all_good = all_good & one_good;
+					all_good = true;
+					for(var i in proposalResponses) {
+						let one_good = false;
+						if (proposalResponses && proposalResponses[i].response && proposalResponses[i].response.status === 200) {
+							one_good = true;
+							logger.info('move proposal was good');
+						} else {
+							logger.error('move proposal was bad');
 						}
+						all_good = all_good & one_good;
 					}
-					if (all_good) {
-						t.pass('Successfully obtained transaction endorsements.'); // + JSON.stringify(proposalResponses));
-						var request = {
-							proposalResponses: proposalResponses,
-							proposal: proposal,
-							header: header
-						};
-						return chain.sendTransaction(request);
-					} else {
-						t.fail('Failed to obtain transaction endorsements. Error code: '
-							+ (results ? results : 'Results are null'));
-						t.end();
-					}
-				},
-				function(err) {
-					t.fail('Failed to send transaction proposal due to error: ' + err.stack ? err.stack : err);
+				}
+				if (all_good) {
+					t.pass('Successfully obtained transaction endorsements.'); // + JSON.stringify(proposalResponses));
+					var request = {
+						proposalResponses: proposalResponses,
+						proposal: proposal,
+						header: header
+					};
+					return chain.sendTransaction(request);
+				} else {
+					t.fail('Failed to obtain transaction endorsements. Error code: '
+						+ (results ? results : 'Results are null'));
 					t.end();
 				}
-			).then(
-				function(response) {
-					if (response.status === 'SUCCESS') {
-						t.pass('Successfully ordered endorsement transaction.');
-						return new Promise((resolve, reject) => {
-							var handle = setTimeout(reject, 30000);
+			},
+			(err) => {
+				t.fail('Failed to send transaction proposal due to error: ' + err.stack ? err.stack : err);
+				t.end();
+			}).then((response) => {
+				if (response.status === 'SUCCESS') {
+					t.pass('Successfully ordered endorsement transaction.');
+					return new Promise((resolve, reject) => {
+						var handle = setTimeout(reject, 30000);
 
-							eh.registerTxEvent(tx_id.toString(), (tx) => {
-								t.pass('The chaincode deploy transaction has been successfully committed');
-								clearTimeout(handle);
+						eh.registerTxEvent(tx_id.toString(), (tx) => {
+							t.pass('The chaincode deploy transaction has been successfully committed');
+							clearTimeout(handle);
 
-								if (!useSteps) {
-									resolve();
-								} else if (steps.length === 1 && steps[0] === 'step2') {
-									t.end();
-									resolve();
-								}
-							});
+							if (!useSteps) {
+								resolve();
+							} else if (steps.length === 1 && steps[0] === 'step2') {
+								t.end();
+								resolve();
+							}
 						});
-					} else {
-						t.fail('Failed to order the endorsement of the transaction. Error code: ' + response.status);
-						t.end();
-					}
-				},
-				function(err) {
-					t.fail('Failed to send transaction proposal due to error: ' + err.stack ? err.stack : err);
+					});
+				} else {
+					t.fail('Failed to order the endorsement of the transaction. Error code: ' + response.status);
 					t.end();
 				}
-			);
+			},
+			(err) => {
+				t.fail('Failed to send transaction proposal due to error: ' + err.stack ? err.stack : err);
+				t.end();
+			});
 		}
 
 		if (!useSteps || steps.indexOf('step3') >= 0) {
-			promise = promise.then(
-				function(data) {
-					logger.info('Executing step3');
+			promise = promise.then((data) => {
+				logger.info('Executing step3');
 
-					// we may get to this point from the sleep() call above
-					// or from skipping step1 altogether. if coming directly
-					// to this step then "data" will be the webUser
-					if (typeof data !== 'undefined' && data !== null) {
-						webUser = data;
-					}
-
-					return;
-				},
-				function(err) {
-					t.fail('Failed to get transaction notification within the timeout period');
-					t.end();
+				// send query
+				var request = {
+					targets: [peer0, peer1],
+					chaincodeId : chaincode_id,
+					chainId: chain_id,
+					txId: utils.buildTransactionID(),
+					nonce: utils.getNonce(),
+					fcn: 'invoke',
+					args: ['query','b']
+				};
+				return chain.queryByChaincode(request);
+			},
+			(err) => {
+				t.fail('Failed to get transaction notification within the timeout period');
+				t.end();
+			}).then((response_payloads) => {
+				for(let i = 0; i < response_payloads.length; i++) {
+					t.equal(response_payloads[i].toString('utf8'),'300','checking query results are correct that user b has 300 now after the move');
 				}
-			).then(
-				function() {
-					// send query
-					var request = {
-						targets: [peer0, peer1],
-						chaincodeId : chaincode_id,
-						chainId: chain_id,
-						txId: utils.buildTransactionID(),
-						nonce: utils.getNonce(),
-						fcn: 'invoke',
-						args: ['query','b']
-					};
-					return chain.queryByChaincode(request);
-				},
-				function(err) {
-					t.fail('Failed to wait-- error: ' + err.stack ? err.stack : err);
-					t.end();
-				}
-			).then(
-				function(response_payloads) {
-					for(let i = 0; i < response_payloads.length; i++) {
-						t.equal(response_payloads[i].toString('utf8'),'300','checking query results are correct that user b has 300 now after the move');
-					}
-					t.end();
-				},
-				function(err) {
-					t.fail('Failed to send query due to error: ' + err.stack ? err.stack : err);
-					t.end();
-				}
-			).catch(
-				function(err) {
-					t.fail('Failed to end to end test with error:' + err.stack ? err.stack : err);
-					t.end();
-				}
-			);
+				t.end();
+			},
+			(err) => {
+				t.fail('Failed to send query due to error: ' + err.stack ? err.stack : err);
+				t.end();
+			}).catch((err) => {
+				t.fail('Failed to end to end test with error:' + err.stack ? err.stack : err);
+				t.end();
+			});
 		}
 	});
 });
