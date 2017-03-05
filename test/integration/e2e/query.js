23844717f4a916aa5e7757a24c2461e07637cded
Jim Zhang <jzhang@us.ibm.com>
2017-03-05 17:18:27 -0500

    Add query to new multi-org end2end test
    FAB-2617
    This is the last step of the new e2e test, making a query to
    verify the invocation transaction result.
    
    Change-Id: I544ede3c8994dbdb3712449a72d97101440cb6f5
    Signed-off-by: Jim Zhang <jzhang@us.ibm.com>

---
+++ test/integration/e2e/query.js
@@ -0,0 +1,114 @@
+/**
+ * Copyright 2017 IBM All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *    http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *  Unless required by applicable law or agreed to in writing, software
+ *  distributed under the License is distributed on an "AS IS" BASIS,
+ *  WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ *  See the License for the specific language governing permissions and
+ *  limitations under the License.
+ */
+
+// This is an end-to-end test that focuses on exercising all parts of the fabric APIs
+// in a happy-path scenario
+'use strict';
+
+var tape = require('tape');
+var _test = require('tape-promise');
+var test = _test(tape);
+
+var path = require('path');
+var util = require('util');
+
+var hfc = require('fabric-client');
+var utils = require('fabric-client/lib/utils.js');
+var Peer = require('fabric-client/lib/Peer.js');
+var Orderer = require('fabric-client/lib/Orderer.js');
+var EventHub = require('fabric-client/lib/EventHub.js');
+var testUtil = require('../../unit/util.js');
+
+var logger = utils.getLogger('install-chaincode');
+
+var e2e = testUtil.END2END;
+hfc.addConfigFile(path.join(__dirname, './config.json'));
+var ORGS = hfc.getConfigSetting('test-network');
+
+var tx_id = null;
+var nonce = null;
+var the_user = null;
+
+test('\n\n***** End-to-end flow: query chaincode *****', (t) => {
+	// this is a transaction, will just use org1's identity to
+	// submit the request. intentionally we are using a different org
+	// than the one that submitted the "move" transaction, although either org
+	// should work properly
+	var org = 'org1';
+	var client = new hfc();
+	var chain = client.newChain(e2e.channel);
+
+	var orgName = ORGS[org].name;
+
+	var targets = [];
+	// set up the chain to use each org's 'peer1' for
+	// both requests and events
+	for (let key in ORGS) {
+		if (ORGS.hasOwnProperty(key) && typeof ORGS[key].peer1 !== 'undefined') {
+			let peer = new Peer(ORGS[key].peer1.requests);
+			chain.addPeer(peer);
+		}
+	}
+
+	return hfc.newDefaultKeyValueStore({
+		path: testUtil.storePathForOrg(orgName)
+	}).then((store) => {
+
+		client.setStateStore(store);
+		return testUtil.getSubmitter(client, t, org);
+
+	}).then((admin) => {
+		the_user = admin;
+		the_user.mspImpl._id = ORGS[org].mspid;
+
+		nonce = utils.getNonce();
+		tx_id = chain.buildTransactionID(nonce, the_user);
+
+		// send query
+		var request = {
+			chaincodeId : e2e.chaincodeId,
+			chaincodeVersion : e2e.chaincodeVersion,
+			chainId: e2e.channel,
+			txId: tx_id,
+			nonce: nonce,
+			fcn: 'invoke',
+			args: ['query','b']
+		};
+		return chain.queryByChaincode(request);
+	},
+	(err) => {
+		t.comment('Failed to get submitter \'admin\'');
+		t.fail('Failed to get submitter \'admin\'. Error: ' + err.stack ? err.stack : err );
+		t.end();
+	}).then((response_payloads) => {
+		if (response_payloads) {
+			for(let i = 0; i < response_payloads.length; i++) {
+				t.equal(response_payloads[i].toString('utf8'),'300','checking query results are correct that user b has 300 now after the move');
+			}
+			t.end();
+		} else {
+			t.fail('response_payloads is null');
+			t.end();
+		}
+	},
+	(err) => {
+		t.fail('Failed to send query due to error: ' + err.stack ? err.stack : err);
+		t.end();
+	}).catch((err) => {
+		t.fail('Failed to end to end test with error:' + err.stack ? err.stack : err);
+		t.end();
+	});
+});
