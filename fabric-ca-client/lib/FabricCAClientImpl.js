d9c870b46471d084d7bd99227df1955b4e0c6915
jjjjibm <jcwolf@us.ibm.com>
2017-03-29 11:39:56 -0400

    node-SDK FAB-2853 test for 'undefined' fails
    I changed some tests like this:
    if (<variable> === 'undefined)
    to
    if (typeof <variable> === 'undefined').
    See FAB-2853 for more discussion.
    
    Change-Id: I8471ba6e8b3ed151ce35ea07732c09ff9a295a49
    Signed-off-by: jjjjibm <jcwolf@us.ibm.com>

--- fabric-ca-client/lib/FabricCAClientImpl.js
+++ fabric-ca-client/lib/FabricCAClientImpl.js
@@ -121,6 +121,10 @@ var FabricCAServices = class {
 		var self = this;
 
 		return new Promise(function (resolve, reject) {
+			if (typeof req === 'undefined' || req === null) {
+				logger.error('enroll() missing required argument "request"');
+				return reject(new Error('Missing required argument "request"'));
+			}
 			if (!req.enrollmentID) {
 				logger.error('Invalid enroll request, missing enrollmentID');
 				return reject(new Error('req.enrollmentID is not set'));
@@ -300,10 +304,10 @@ var FabricCAClient = class {
 			};
 		} else {
 			this._tlsOptions = connect_opts.tlsOptions;
-			if (this._tlsOptions.verify==='undefined'){
+			if (typeof this._tlsOptions.verify === 'undefined') {
 				this._tlsOptions.verify = true;
 			}
-			if (this._tlsOptions.trustedRoots==='undefined'){
+			if (typeof this._tlsOptions.trustedRoots === 'undefined') {
 				this._tlsOptions.trustedRoots = [];
 			}
 		}
