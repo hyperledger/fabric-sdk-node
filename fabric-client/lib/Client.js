f426239fa701648bbc30463440f5ee18d355cede
Eric Vaughn <eric.vaughn@blocledger.com>
2017-07-25 15:29:12 -0400

    [FAB-5409] JSDoc, Add targets to installChaincode() req
    Add a targets parameter to the JSDoc for ChaincodeInstallRequest
    used by installChaincode()
    
    Change-Id: Ifdd7cc4e498609e5898652db37ea23676daeaeae
    Signed-off-by: Eric Vaughn <eric.vaughn@blocledger.com>

--- fabric-client/lib/Client.js
+++ fabric-client/lib/Client.js
@@ -625,6 +625,8 @@ var Client = class extends BaseClient {
 
 	/**
 	 * @typedef {Object} ChaincodeInstallRequest
+	 * @property {Peer[]} targets - Required. An array of Peer objects that the chaincode will
+	 *                              be installed on
 	 * @property {string} chaincodePath - Required. The path to the location of the source code
 	 *                                    of the chaincode. If the chaincode type is golang, then
 	 *                                    this path is the fully qualified package name, such as
