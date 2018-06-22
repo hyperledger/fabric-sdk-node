cce2a384b66d4b89f807929c66a4792096fb9219
wenjian3 <wenjianq@gmail.com>
2018-06-22 15:00:25 -0400

    FAB-10808 NodeSDK - add private-data.js to e2e.js
    Add private-data.js to e2e.js and remove it from test.js
    so that it will not run twice.
    Remove e2e/collections-config.json because it is moved to example_cc_private.
    
    Change-Id: I794cf806c25565a7a6a6005b31d935c5353857fb
    Signed-off-by: Wenjian Qiao <wenjianq@gmail.com>

--- test/integration/e2e.js
+++ test/integration/e2e.js
@@ -10,4 +10,5 @@ require('./e2e/install-chaincode.js');
 require('./e2e/instantiate-chaincode.js');
 require('./e2e/invoke-transaction.js');
 require('./e2e/query.js');
+require('./e2e/private-data.js');
 require('./e2e/upgrade.js');
