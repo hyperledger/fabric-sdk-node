44a8b1735320fd0bf0aa9cc3baa269748e75d836
Mark S. Lewis <mark_lewis@uk.ibm.com>
2019-03-12 11:59:48 +0000

    FABN-1176: Fix Mac crypto generation scripts
    Change-Id: I366316a02dd5b7d063c714693c5218c6b6a15682
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- test/fixtures/crypto-material/config-base/generate.sh
+++ test/fixtures/crypto-material/config-base/generate.sh
@@ -4,34 +4,34 @@
 #
 
 # Set the path to teh crypto material to ensure it may be used
-CRYPTOGEN=$1
-export PATH=${PATH}:${CRYPTOGEN}
+CRYPTOGEN="$1"
+export PATH="${PATH}:${CRYPTOGEN}"
 
 # Get current location to ensure things go to the correct place
-BASEDIR=$(dirname $(realpath $0))
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 # Start generating
 echo "Creating new crypto material and tx blocks from within directory ${BASEDIR}"
-rm -rf ${BASEDIR}/../crypto-config
-rm -rf ${BASEDIR}/../channel-config
-mkdir ${BASEDIR}/../crypto-config
-mkdir ${BASEDIR}/../channel-config
+rm -rf "${BASEDIR}/../crypto-config"
+rm -rf "${BASEDIR}/../channel-config"
+mkdir "${BASEDIR}/../crypto-config"
+mkdir "${BASEDIR}/../channel-config"
 
 echo 'Generating base crypto-material and channel tx files....'
-export FABRIC_CFG_PATH=${BASEDIR}
-cryptogen generate --config=${BASEDIR}/crypto-config.yaml --output=${BASEDIR}/../crypto-config
-configtxgen -profile TwoOrgsOrdererGenesis -outputBlock ${BASEDIR}/twoorgs.genesis.block
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ${BASEDIR}/../channel-config/mychannel.tx -channelID mychannel
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ${BASEDIR}/../channel-config/mychannel2.tx -channelID mychannel2 #test/integration/network-config.js
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ${BASEDIR}/../channel-config/mychanneltx.tx -channelID mychanneltx #test/integration/create-configtx-channel.js
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ${BASEDIR}/../channel-config/mychannelts.tx -channelID mychannelts #test/typescript
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ${BASEDIR}/../channel-config/adminconfig.tx -channelID adminconfig #test/only-admin.js
-configtxgen -profile TwoOrgsChannel -outputCreateChannelTx ${BASEDIR}/../channel-config/discovery.tx -channelID discovery #test/integration/discovery.js
+export FABRIC_CFG_PATH="${BASEDIR}"
+cryptogen generate --config="${BASEDIR}/crypto-config.yaml" --output="${BASEDIR}/../crypto-config"
+configtxgen -profile TwoOrgsOrdererGenesis -outputBlock "${BASEDIR}/twoorgs.genesis.block"
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx "${BASEDIR}/../channel-config/mychannel.tx" -channelID mychannel
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx "${BASEDIR}/../channel-config/mychannel2.tx" -channelID mychannel2 #test/integration/network-config.js
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx "${BASEDIR}/../channel-config/mychanneltx.tx" -channelID mychanneltx #test/integration/create-configtx-channel.js
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx "${BASEDIR}/../channel-config/mychannelts.tx" -channelID mychannelts #test/typescript
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx "${BASEDIR}/../channel-config/adminconfig.tx" -channelID adminconfig #test/only-admin.js
+configtxgen -profile TwoOrgsChannel -outputCreateChannelTx "${BASEDIR}/../channel-config/discovery.tx" -channelID discovery #test/integration/discovery.js
 
 echo 'Generating crypto-material complete, now renaming keys...'
 # Rename the key files we use to be key.pem instead of a uuid
-for KEY in $(find ${BASEDIR}/../crypto-config -type f -name "*_sk"); do
-    KEY_DIR=$(dirname ${KEY})
-    mv ${KEY} ${KEY_DIR}/key.pem
+for KEY in $(find "${BASEDIR}/../crypto-config" -type f -name "*_sk"); do
+    KEY_DIR="$(dirname ${KEY})"
+    mv ${KEY} "${KEY_DIR}/key.pem"
 done
 echo 'Renaming keys complete'
--- test/fixtures/fabricca/generateCSR.sh
+++ test/fixtures/fabricca/generateCSR.sh
@@ -1,6 +1,10 @@
+#!/bin/bash
+#
+# SPDX-License-Identifier: Apache-2.0
+#
 
-BASEDIR=$(dirname $(realpath $0))
+BASEDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 echo "Generating fabricca cetificates and keys in directory ${BASEDIR}"
-openssl req -nodes -newkey rsa:2048 -keyout ${BASEDIR}/test.key -out ${BASEDIR}/test.csr -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=aTestUser"
-openssl req -nodes -newkey rsa:512 -keyout ${BASEDIR}/enroll-key.pem -out ${BASEDIR}/enroll-csr.pem -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=testUser"
+openssl req -nodes -newkey rsa:2048 -keyout "${BASEDIR}/test.key" -out "${BASEDIR}/test.csr" -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=aTestUser"
+openssl req -nodes -newkey rsa:512 -keyout "${BASEDIR}/enroll-key.pem" -out "${BASEDIR}/enroll-csr.pem" -subj "/C=GB/ST=London/L=London/O=Global Security/OU=IT Department/CN=testUser"
 echo "Generation complete"
