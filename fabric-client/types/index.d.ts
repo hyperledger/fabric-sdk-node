b863a7ad03db2d6e1558f2a51094282bd9ce3818
Simon Stone <sstone1@uk.ibm.com>
2019-11-22 14:47:10 +0000

    [FABN-1407] Add PDC and EP types (#28)
    Add typings for a private data collection configuration
    and endorsmeent policy to ChaincodeInstantiateUpgradeRequest.
    
    Signed-off-by: Simon Stone <sstone1@uk.ibm.com>
    Change-Id: I32f1789dbb96fecc88d03fcd841356d21fcb89e9

--- fabric-client/types/index.d.ts
+++ fabric-client/types/index.d.ts
@@ -475,17 +475,35 @@ declare namespace Client { // tslint:disable-line:no-namespace
 
 	export type ChaincodeInstallRequest = ChaincodePackageInstallRequest | ChaincodePathInstallRequest;
 
+	export interface CollectionConfig {
+		name: string;
+		policy: {
+			identities: any[];
+			policy: any;
+		};
+		requiredPeerCount: number;
+		maxPeerCount: number;
+		blockToLive?: number;
+		memberOnlyRead?: boolean;
+	}
+
+	export type CollectionsConfig = CollectionConfig[];
+
+	export interface EndorsementPolicy {
+		identities: any[];
+		policy: any;
+	}
 	export interface ChaincodeInstantiateUpgradeRequest {
 		targets?: Peer[] | string[];
 		chaincodeType?: ChaincodeType;
 		chaincodeId: string;
 		chaincodeVersion: string;
 		txId: TransactionId;
-		'collections-config'?: string;
+		'collections-config'?: string | CollectionsConfig;
 		transientMap?: TransientMap;
 		fcn?: string;
 		args?: string[];
-		'endorsement-policy'?: any;
+		'endorsement-policy'?: EndorsementPolicy;
 	}
 
 	export interface ChaincodeInvokeRequest {
