558cea43f81a5c21ec04e55fe972e1afcee26dff
Mark S. Lewis <Mark.S.Lewis@outlook.com>
2019-12-13 10:22:02 +0000

    Wallet interop test (#69)
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

---
+++ fabric-network/test/impl/wallet/wallet-interop.spec.ts
@@ -0,0 +1,40 @@
+/**
+ * Copyright 2019 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+import { Identity } from '../../../src/impl/wallet/identity';
+import { Wallets } from '../../../src/impl/wallet/wallets';
+import { Wallet } from '../../../src/impl/wallet/wallet';
+
+import path = require('path');
+
+import { expect } from 'chai';
+
+// tslint:disable: no-unused-expression
+
+const testWalletPath = path.join(__dirname, '..', '..', '..', '..', 'test', 'fixtures', 'test-wallet');
+
+describe('Wallet interop', () => {
+	let wallet: Wallet;
+	let labels: string[];
+
+	before(async () => {
+		wallet = await Wallets.newFileSystemWallet(testWalletPath);
+		labels = await wallet.list();
+	});
+
+	it('Wallet is not empty', () => {
+		expect(labels).to.not.be.empty;
+	});
+
+	it('Read identities', async () => {
+		for (const label of labels) {
+			const identity = await wallet.get(label) as Identity;
+			expect(identity).to.exist;
+			expect(identity.mspId).to.not.be.empty;
+			expect(identity.type).to.not.be.empty;
+		}
+	});
+});
