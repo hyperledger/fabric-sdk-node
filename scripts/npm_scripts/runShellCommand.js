2f64d0f7286938d621f755fadf7790245acf765e
Arnaud J Le Hors <lehors@us.ibm.com>
2020-04-30 11:10:10 +0200

    Add missing copyright to several files
    This adds the missing copyright and changes the license to SPDX.
    
    Signed-off-by: Arnaud J Le Hors <lehors@us.ibm.com>

--- scripts/npm_scripts/runShellCommand.js
+++ scripts/npm_scripts/runShellCommand.js
@@ -1,6 +1,8 @@
 /*
-# SPDX-License-Identifier: Apache-2.0
-*/
+ * Copyright IBM Corp. All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
 
 const childProcess = require('child_process');
 const stripAnsi = require('strip-ansi');
