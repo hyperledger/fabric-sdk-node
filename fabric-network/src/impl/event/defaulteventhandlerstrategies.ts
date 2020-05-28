6f15e702be11e8c2dbb7d435b1730ac11033f1cd
Mark S. Lewis <mark_lewis@uk.ibm.com>
2020-05-28 11:26:27 +0100

    FABN-1565: Fix docs for DefaultEventHandlerStrategies
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- fabric-network/src/impl/event/defaulteventhandlerstrategies.ts
+++ fabric-network/src/impl/event/defaulteventhandlerstrategies.ts
@@ -26,12 +26,12 @@ function getNetworkPeers(network: Network): Endorser[] {
  * events from all peers in the client identity's organization.
  * The [submitTransaction]{@link module:fabric-network.Contract#submitTransaction} function will wait until successful
  * events are received from <em>all</em> currently connected peers (minimum 1).
- * @property {module:fabric-network.TxEventHandlerFactory} MSPID_SCOPE_ALLFORTX Listen for transaction commit
+ * @property {module:fabric-network.TxEventHandlerFactory} MSPID_SCOPE_ANYFORTX Listen for transaction commit
  * events from all peers in the client identity's organization.
- * The [submitTransaction]{@link module:fabric-network.Contract#submitTransaction} function will wait until successful
- * events are received from <em>all</em> currently connected peers (minimum 1).
- * @property {module:fabric-network.TxEventHandlerFactory} MSPID_SCOPE_ALLFORTX Listen for transaction commit
- * events from all peers in the client identity's organization.
+ * The [submitTransaction]{@link module:fabric-network.Contract#submitTransaction} function will wait until a successful
+ * event is received from <em>any</em> peer.
+ * @property {module:fabric-network.TxEventHandlerFactory} NETWORK_SCOPE_ALLFORTX Listen for transaction commit
+ * events from all peers in the network.
  * The [submitTransaction]{@link module:fabric-network.Contract#submitTransaction} function will wait until successful
  * events are received from <em>all</em> currently connected peers (minimum 1).
  * @property {module:fabric-network.TxEventHandlerFactory} NETWORK_SCOPE_ANYFORTX Listen for transaction commit
