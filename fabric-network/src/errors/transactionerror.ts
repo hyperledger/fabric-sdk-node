e71b12600a99620d32da6df7a820e74356f5a141
harrisob <beharrison@nc.rr.com>
2020-06-25 11:03:50 -0400

    FABN-1566 Add TransactionError with event status (#258)
    When a transaction completes with a non-valid status
    the error object will be of type TransactionError which
    will include the transactionCode and transactionId attribute.
    
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>

---
+++ fabric-network/src/errors/transactionerror.ts
@@ -0,0 +1,35 @@
+/**
+ * Copyright 2020 IBM All Rights Reserved.
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+'use strict';
+
+import { FabricError } from './fabricerror';
+
+export interface TransactionErrorInfo {
+	message: string;
+	transactionId: string;
+	transactionCode: string;
+}
+
+/**
+ * Base type for Fabric-specific errors.
+ * @memberof module:fabric-network
+ * @property {string} [transactionId] ID of the associated transaction.
+ * @property {string} [transactionCode] The transaction validation code of the associated transaction.
+ */
+export class TransactionError extends FabricError {
+	transactionCode?: string;
+
+	/*
+	 * Constructor.
+	 * @param {(string|object)} [info] Either an error message (string) or additional properties to assign to this
+	 * instance (object).
+	 */
+	constructor(info?: string | TransactionErrorInfo) {
+		super(info);
+		this.name = TransactionError.name;
+	}
+}
