d30852a08b6074f161048442299619a3e87bb83b
Bret Harrison <beharrison@nc.rr.com>
2021-01-14 13:33:22 -0500

    FABN-1641 gateway connection-options pass to low level
    fix issues with the fabtric-network
    
    Signed-off-by: Bret Harrison <beharrison@nc.rr.com>

--- fabric-network/src/gateway.ts
+++ fabric-network/src/gateway.ts
@@ -275,7 +275,7 @@ export class Gateway {
 		};
 
 		this.options = mergeOptions(defaultOptions, options);
-		logger.debug('connection options: %j', options);
+		logger.debug('gateway options: %j', options);
 
 		let loadCcp = false;
 		if (config instanceof Client) {
@@ -328,7 +328,7 @@ export class Gateway {
 		// of connection options for an endpoint
 		// these will be merged with those from the config (default.json)
 		if (options['connection-options']) {
-			(this.client as any).centralized_options = options['connection-options'];
+			this.client.setCentralizedConnectionOptions(options['connection-options']);
 			logger.debug('%s - assigned connection options');
 		}
 
