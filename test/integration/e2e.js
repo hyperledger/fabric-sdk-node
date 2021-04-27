738539b845cf6a2e253126af6bc98c54dfca277d
David Liu <david.yx.liu@oracle.com>
2021-04-27 17:34:35 +0800

    [FABN-1711] prune deprecated fabric-client (#449)
    May will be EOL of 1.4 LTS
    
    Signed-off-by: DavidLiu <david.yx.liu@oracle.com>

--- test/integration/e2e.js
+++
@@ -1,8 +0,0 @@
-/*
- Copyright IBM Corp. All Rights Reserved.
-
- SPDX-License-Identifier: Apache-2.0
-*/
-require('./e2e/create-channel.js');
-require('./e2e/join-channel.js');
-require('./e2e/updateAnchorPeers.js');
--- test/integration/network-e2e/e2e-hsm.js
+++
@@ -1,9 +0,0 @@
-/**
- * Copyright 2017 IBM All Rights Reserved.
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
-require('./install-chaincode.js');
-require('./instantiate-chaincode.js');
-require('./invoke-hsm.js');
