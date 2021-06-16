8358820191691b85625d4ac683d9f2690bf1f787
Mark S. Lewis <mark_lewis@uk.ibm.com>
2021-06-16 14:54:25 +0100

    FABN-1714: Allow transactions to be serialized
    Transaction state can be serialized using Transaction.serialize(), and later reconstructed using Contract.deserializeTransaction(). This allows a transaction to be persisted and for the same transaction (including nonce and transaction ID) to be submitted again following a client application restart.
    
    Signed-off-by: Mark S. Lewis <mark_lewis@uk.ibm.com>

--- fabric-common/types/index.d.ts
+++ fabric-common/types/index.d.ts
@@ -399,6 +399,7 @@ export class Channel {
 
 export class IdentityContext {
 	readonly transactionId: string;
+	readonly nonce: Buffer;
 	constructor(user: User, client: Client);
 	calculateTransactionId(): IdentityContext;
 	serializeIdentity(): Buffer;
