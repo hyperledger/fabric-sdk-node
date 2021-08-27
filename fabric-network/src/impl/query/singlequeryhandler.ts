80ec568d3f6fd72c1a1d0b00eaf90cc866e86d57
90felipe09 <Felhipeshiraishi@gmail.com>
2021-08-27 08:11:54 -0300

    add: more clear exception log error for no peers
    Signed-off-by: 90felipe09 <Felhipeshiraishi@gmail.com>

--- fabric-network/src/impl/query/singlequeryhandler.ts
+++ fabric-network/src/impl/query/singlequeryhandler.ts
@@ -28,6 +28,13 @@ export class SingleQueryHandler implements QueryHandler {
 		const method = 'evaluate';
 		logger.debug('%s - start', method);
 
+		if (this.peers.length == 0){
+			const message = util.format('Query failed. No available endorser peers.');
+			const error = new FabricError(message);
+			logger.error('evaluate:', error);
+			throw error;
+		}
+
 		const startPeerIndex = this.currentPeerIndex;
 		const errorMessages = [];
 
